3ca3feb561bb4729d25f8e5470ff0c7d8dfa6583
Hector Niñez J <desarrolloPerrosky@github.com>
2020-09-11 05:36:47 -0300

    Objetivos tarea uno explicitos

--- app/src/main/java/com/example/testaplication/MainActivity.java
+++ app/src/main/java/com/example/testaplication/MainActivity.java
@@ -5,21 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int transversal = 0;
+
+    private int inicial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inicial = 0;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        TextView textView2 =  findViewById(R.id.textView2);
+        String valor = textView2.getText() + " :: " + ++inicial;
+        textView2.setText(valor);
+
+        TextView textView3 =  findViewById(R.id.textView3);
+        String valor2 = textView3.getText() + " :: " + ++transversal;
+        textView3.setText(valor);
     }
 
     public void abrirActividad(View view) {
-        Toast.makeText(this, "Nadi de nadi", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, R.string.subtitulo1, Toast.LENGTH_LONG).show();
         Intent intent = new Intent(this, DosActivity.class);
         startActivity(intent);
         //finish();
-        //Toast.makeText(this, "Terminé", Toast.LENGTH_LONG).show();
     }
 }
