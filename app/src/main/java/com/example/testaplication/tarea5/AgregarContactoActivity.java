15c4833a76070f426e03c7e66707a413be20c47e
grisone <recepcion.correos.fp@gmail.com>
2020-11-06 01:46:11 -0300

    validacion formulario contacto

--- app/src/main/java/com/example/testaplication/tarea5/AgregarContactoActivity.java
+++ app/src/main/java/com/example/testaplication/tarea5/AgregarContactoActivity.java
@@ -1,17 +1,23 @@
 package com.example.testaplication.tarea5;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.testaplication.R;
+import com.example.testaplication.SegundaPantalla;
 
 public class AgregarContactoActivity extends AppCompatActivity {
 
@@ -40,10 +46,36 @@ public class AgregarContactoActivity extends AppCompatActivity {
                 onBackPressed();
                 break;
             case R.id.guardar:
-                Toast.makeText(getApplicationContext(), "Guargdar Pendiente de validacion", Toast.LENGTH_LONG).show();
+                if (validarContacto()){
+                    Toast.makeText(getApplicationContext(), "Datos guardados correctamente", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Debe ingresar todos los datos", Toast.LENGTH_LONG).show();
+
+                }
+
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean validarContacto(){
+        boolean valido = false;
+        EditText edtNombre = (EditText) findViewById(R.id.nombres);
+        EditText edtPaterno = (EditText) findViewById(R.id.ap_paterno);
+        EditText edtMaterno = (EditText) findViewById(R.id.ap_materno);
+        EditText edtFono = (EditText) findViewById(R.id.fono);
+
+
+
+        if(!edtNombre.getText().toString().isEmpty() &&
+                !edtPaterno.getText().toString().isEmpty() &&
+                !edtMaterno.getText().toString().isEmpty() &&
+                !edtFono.getText().toString().isEmpty()
+
+        ){
+            valido=true;
+        }
+    return valido;
     }
 
 }
